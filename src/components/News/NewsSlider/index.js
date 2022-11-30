dd4f248e9f25b4376957c21b045f5b6b30e823c1
pradeep <pradeep.kunchala15@gmail.com>
2022-11-30 20:46:35 +0530

    initial commit

--- src/components/News/NewsSlider/index.js
+++ src/components/News/NewsSlider/index.js
@@ -4,6 +4,7 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import Newsfetch from "../";
 
 const News = ({ news }) => {
   return (
