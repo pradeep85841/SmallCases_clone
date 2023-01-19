84dd04b09fba5d6e54dc1aa5bf6b8a1005d052b4
pradeep <pradeep.kunchala15@gmail.com>
2023-01-19 15:13:40 +0530

    BASE_URL added

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import News from "./NewsSlider/index.js";
+import BASE_URL from "../../Base.js";
 
 const Newsfetch = () => {
   const [news, setNews] = useState([]);
   useEffect(() => {
-    fetch("/posts")
+    fetch(`${BASE_URL}/posts`)
       .then((res) => res.json())
       .then((res) => {
         setNews(res[0].data);
