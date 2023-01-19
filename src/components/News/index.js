45383d2e5e5b2ac4e7be4fd201790bfc788b3828
pradeep <pradeep.kunchala15@gmail.com>
2023-01-19 17:16:33 +0530

    msg

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -4,7 +4,7 @@ import News from "./NewsSlider/index.js";
 const Newsfetch = () => {
   const [news, setNews] = useState([]);
   useEffect(() => {
-    fetch("3.93.45.63:5000/posts")
+    fetch("ec2-3-93-45-63.compute-1.amazonaws.com/posts")
       .then((res) => res.json())
       .then((res) => {
         setNews(res[0].data);
