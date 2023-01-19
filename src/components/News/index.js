18722fc432008b37ae33ad1da70a803bb650e30a
pradeep <pradeep.kunchala15@gmail.com>
2023-01-19 18:17:29 +0530

    .ENV

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import News from "./NewsSlider/index.js";
+const BASE_URL = process.env.BASE_URL;
 
 const Newsfetch = () => {
   const [news, setNews] = useState([]);
   useEffect(() => {
-    fetch("ec2-3-93-45-63.compute-1.amazonaws.com/posts")
+    fetch(`${BASE_URL}/posts`)
       .then((res) => res.json())
       .then((res) => {
         setNews(res[0].data);
