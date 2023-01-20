29457310c981ceda77fbab911f6f8afbe6c76df9
pradeep <pradeep.kunchala15@gmail.com>
2023-01-20 19:57:49 +0530

    added both

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,5 @@
+//const proxy = require("http-proxy-middleware");
 import proxy from "http-proxy-middleware";
 module.exports = function (app) {
-  app.use(
-    proxy("/api", {
-      target: "https://ec2-54-226-68-179.compute-1.amazonaws.com",
-    })
-  );
+  app.use(proxy("/api", { target: process.env.REACT_APP_BACKEND_URL }));
 };
