d700c4ec4d309a997312f9438dc746bf4f6fbf1c
pradeep <pradeep.kunchala15@gmail.com>
2023-01-21 11:19:16 +0530

    added cert

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,17 @@
 //const proxy = require("http-proxy-middleware");
 import proxy from "http-proxy-middleware";
-module.exports = function (app) {
-  app.use(proxy("/api", { target: process.env.REACT_APP_BACKEND_URL }));
-};
+const https = require("https");
+
+app.use(
+  proxy("/api", {
+    target: process.env.REACT_APP_BACKEND_URL,
+    secure: true,
+    changeOrigin: true,
+    agent: new https.Agent({
+      cert: fs.readFileSync("./cert/server_root.cert.pem"),
+      key: fs.readFileSync("./cert/server_root.key.pem"),
+      passphrase: "pesto",
+      ca: fs.readFileSync("./cert/server.cert.pem"),
+    }),
+  })
+);
