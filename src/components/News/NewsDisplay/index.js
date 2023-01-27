2e9d435bfc01c579fd6a407ae7985f5adf8a858e
pradeep <pradeep.kunchala15@gmail.com>
2023-01-27 16:06:29 +0530

    key changed

--- src/components/News/NewsDisplay/index.js
+++ src/components/News/NewsDisplay/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./index.css";
-const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 const NewsDisplay = () => {
   const [search, setSearch] = useState("");
