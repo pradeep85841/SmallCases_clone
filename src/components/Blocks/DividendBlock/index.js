b0f17cf5127b4b50d7570ac524ba7f6d413b57ee
pradeep <pradeep.kunchala15@gmail.com>
2023-02-03 15:42:40 +0530

    div code header removed

--- src/components/Blocks/DividendBlock/index.js
+++ src/components/Blocks/DividendBlock/index.js
@@ -14,9 +14,6 @@ export function getData() {
     const payload = {
       method: "POST",
       body: JSON.stringify({ blockName: `${value}` }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
     };
     fetch(`${BASE_URL}/blockEstimate`, payload)
       .then((res) => res.json())
